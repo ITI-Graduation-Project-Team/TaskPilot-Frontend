27832254d00e289e28d0851f7f1bef01fa72532b
abddo <abdoabdelhay110@gmail.com>
2026-06-20 11:15:28 +0300

    feat: implement global loading indicator with interceptor and service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+
+import { LoaderComponent } from './shared/ui/loader/loader';
+import { LoadingService } from './shared/services/loading.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`,
+  imports: [
+    RouterOutlet,
+    LoaderComponent,
+    AsyncPipe
+  ],
+  template: `
+    @if ((loading$ | async) === true) {
+      <app-loader />
+    }
+
+    <router-outlet />
+  `,
 })
-export class AppComponent {}
+export class AppComponent {
+  private loadingService = inject(LoadingService);
+
+  loading$ = this.loadingService.loading$;
+}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners
+} from '@angular/core';
+
+import {
+  provideHttpClient,
+  withInterceptors
+} from '@angular/common/http';
+
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { loadingInterceptor } from '../app/shared/api/interceptors/loading-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    provideHttpClient(
+      withInterceptors([
+        loadingInterceptor
+      ])
+    )
   ]
 };
