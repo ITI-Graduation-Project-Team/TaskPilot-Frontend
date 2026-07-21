670703c5867966692916476816ad1068335a0542
Doha-Fawzy <dohasaeed2003@gmail.com>
2026-07-21 12:24:01 +0300

    added commands to README.md

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://taskpilotapi.runasp.net/api',
+  apiUrl: 'https://taskpilotapi.runasp.net/api',
    hubUrl: 'https://taskpilotapi.runasp.net/hubs/notifications',
-   apiUrl: 'https://127.0.0.1:9443/api',
+  //  apiUrl: 'https://127.0.0.1:9443/api',
   auth: {
     tokenKey: 'taskpilot_token',
     refreshTokenKey: 'taskpilot_refreshToken',
